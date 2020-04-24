ca2ebe6794ddd0439a69d46b81cd05b2e6cecee6
Nikita Patil <nikitapatil1122@gmail.com>
2020-04-24 19:56:29 +0530

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+using DatingApp.API.Controllers.Models;
+using Microsoft.EntityFrameworkCore;
+
+//EF core setup 
+//https://docs.microsoft.com/en-us/ef/core/get-started/?tabs=netcore-cli
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
